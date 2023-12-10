46e9b463f008f0a749326859ccf3c4d0335da543
simharamprasad <79112222+simharamprasad@users.noreply.github.com>
2023-12-09 19:40:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-import psycopg2
+
 
 """
 # Welcome to Streamlit!
@@ -13,58 +13,71 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
-# Streamlit App Title
-st.title("PostgreSQL Query App")
 
-# Function to establish a database connection
-@st.cache(allow_output_mutation=True)
+# streamlit_app.py
+
+import streamlit as st
+import pandas as pd
+import psycopg2
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
 def init_connection():
-    return psycopg2.connect(
-        host="your_database_host",
-        user="your_database_user",
-        password="your_database_password",
-        database="your_database_name",
-    )
+    return psycopg2.connect(**st.secrets["postgres"])
 
-# Function to execute a query and return results as a DataFrame
-def run_query(query):
+conn = init_connection()
+
+def get_data(query):
     with conn.cursor() as cur:
         cur.execute(query)
         data = cur.fetchall()
-        columns = [desc[0] for desc in cur.description]
-        df = pd.DataFrame(data, columns=columns)
+        # Convert the query results to a pandas DataFrame
+        df = pd.DataFrame(data, columns=[desc[0] for desc in cur.description])
         return df
+# Use Streamlit to display the retrieved data
+st.header("Farmers Market Database")
+st.write("Enter the query which you want to retrieve data from the database:")
 
-# Connect to the database
-conn = init_connection()
-
-# Display a text input for the user to enter a SQL query
-query = st.text_area("Enter your SQL query here:")
-
-# Check if a query is provided
+query = st.text_input("Query:")
 if query:
-    # Execute the query and display the results
-    st.write("Query Results:")
-    try:
-        result_df = run_query(query)
-        st.dataframe(result_df)
-    except Exception as e:
-        st.error(f"Error executing the query: {str(e)}")
-
-# Display a list of tables in the database
-st.header("Tables in the Database")
+    df = get_data(query)
+    if not df.empty:
+        st.dataframe(df)
+    else:
+        st.write("No data found.")
+# Retrieve all table names from the database
 with conn.cursor() as cur:
     cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
     table_names = [name[0] for name in cur.fetchall()]
 
-selected_table = st.selectbox("Select a table:", table_names)
+# Display a dropdown menu with the table names
+table_name = st.selectbox("Select the table Name:", table_names)
 
-# Display the table content if a table is selected
-if selected_table:
-    st.write(f"Displaying data from table: {selected_table}")
-    table_query = f"SELECT * FROM {selected_table} LIMIT 10;"
-    try:
-        table_df = run_query(table_query)
-        st.dataframe(table_df)
-    except Exception as e:
-        st.error(f"Error retrieving data from the table: {str(e)}")
+# If a table has been selected, display the data in a dataframe
+if table_name:
+    st.write(f"Displaying data from table: {table_name}")
+    query = f"SELECT * FROM {table_name}"
+    df = get_data(query)
+    if not df.empty:
+        st.dataframe(df)
+    else:
+        st.write("No data found.")
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+# @st.cache_data(ttl=600)
+# def run_query(query):
+#     with conn.cursor() as cur:
+#         cur.execute(query)
+#         return cur.fetchall()
+
+# rows = run_query("SELECT * from countries;")
+
+# Print results.
+# for row in rows:
+#     st.write(f"{row[0]} has a :{row[1]}:")
+
+
+
+
+       
